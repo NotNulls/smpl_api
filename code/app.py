c2009850022b167aaf689ec6b6f13699780838fd
Dusan Rangelov <dusanrangelov@yahoo.com>
2023-02-13 08:31:13 +0100

    Create store API endpoints

--- code/app.py
+++ code/app.py
@@ -8,6 +8,7 @@ except ImportError:
     from collections import Mapping
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///data.db'
@@ -28,10 +29,11 @@ jwt = JWT(app, authenticate, identity)
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList,'/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 
 if __name__ == "__main__":
     from db import db
     db.init_app(app)
     app.run(debug=True)
-
 
